0ccd44c7afaf8035c1bf26c0deaeb4f2345dc12b
NotGrafik <pablo.jeanlouis@ynov.com>
2025-05-12 17:04:38 +0200

    :rocket: enhance WeatherActivity with hourly forecast display and weather icon loading; refactor layout to use FrameLayout and add glass background effect

---
+++ app/src/main/java/com/example/smartweatherremind/data/model/ForecastDay.java
@@ -0,0 +1,9 @@
+package com.example.smartweatherremind.data.model;
+
+import java.util.List;
+
+public class ForecastDay {
+    public String date;
+    public List<Hour> hour;
+}
+
---
+++ app/src/main/java/com/example/smartweatherremind/data/model/Hour.java
@@ -0,0 +1,7 @@
+package com.example.smartweatherremind.data.model;
+
+public class Hour {
+    public String time;
+    public double temp_c;
+    public Condition condition;
+}
