40b9164225806268ec9fe81e4808b3fac89964cb
NotGrafik <pablo.jeanlouis@ynov.com>
2025-05-14 09:57:47 +0200

    :rocket: add notification / reminders system with firebase  fonctionnality finally working

--- app/src/main/java/com/example/smartweatherremind/reminder/repository/ReminderRepository.java
+++ app/src/main/java/com/example/smartweatherremind/reminder/repository/ReminderRepository.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
@@ -87,11 +88,19 @@ public class ReminderRepository {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             if (!alarmManager.canScheduleExactAlarms()) {
-                Log.w("Reminder", "Impossible de planifier une alarme exacte : permission refusée dans les paramètres.");
-                Toast.makeText(context, "Veuillez autoriser les alarmes exactes dans les paramètres.", Toast.LENGTH_LONG).show();
+                new Handler(Looper.getMainLooper()).post(() ->
+                        Toast.makeText(context, "Autorisez les alarmes exactes dans les paramètres de l'application.", Toast.LENGTH_LONG).show()
+                );
+
+                Intent intent = new Intent(android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                intent.setData(Uri.parse("package:" + context.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
                 return;
             }
         }
+
+
         Intent intent = new Intent(context, ReminderReceiver.class);
         intent.putExtra("title", reminder.title);
 
